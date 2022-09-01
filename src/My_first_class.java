565eb9669e87e07d82a082fd3f1029b21cb31f91
100133917 <100133917@lubbockisd.net>
2022-09-01 08:28:25 -0500

    changed wonderful to bad

--- src/My_first_class.java
+++ src/My_first_class.java
@@ -1,6 +1,6 @@
 public class My_first_class {
     public static void main(String[] args) {
         System.out.print("Hello world!");
-        System.out.println("its going to be a wonderful day");
+        System.out.println("its going to be a bad day");
     }
 }
