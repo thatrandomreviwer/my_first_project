86d82b892645f38e0ac9557a5defb9f845cfbd61
Parker Tipton <100133917@lubbockisd.net>
2022-09-01 08:23:09 -0500

    Initial commit

---
+++ src/My_first_class.java
@@ -0,0 +1,6 @@
+public class My_first_class {
+    public static void main(String[] args) {
+        System.out.print("Hello world!");
+        System.out.println("its going to be a wonderful day");
+    }
+}
