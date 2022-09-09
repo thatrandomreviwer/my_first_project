500abff0c2cdca7d40d67cea96de3a91c99dc1c3
100133917 <100133917@lubbockisd.net>
2022-09-09 08:52:41 -0500

    notes

--- src/My_first_class.java
+++ src/My_first_class.java
@@ -1,6 +1,43 @@
 public class My_first_class {
     public static void main(String[] args) {
-        System.out.print("Hello world!");
-        System.out.println("its going to be a bad day");
+        System.out.println("hello world");
+
+
+        // four integer-based types
+
+
+        byte value = 20;  //values range from  -128 - 127                       8-bit
+        short anotherValue = 1596; //values range from -32768 - 32767           16 bit
+        int mostPopularValue = 42; //values range from -2147483648 -2147483647  32-bit
+        long extremelyBigNum = -812; // range from -9quintillion to 9quintillion 64-bit
+
+        //2 real number types
+
+        float realValue = 3.6F; //values range from -big to big with 7 significant digits 32-bit
+        double popularRealValue = 324.8643; //"""" with 14 significant digits             64-bit
+
+
+        //boolean (ture/false)
+
+        boolean lightSwitch = true; //only values of true or false
+
+
+        //character data -- a single character
+
+
+        char singleChacter = '#'; // unicode 65535 integers
+                               //A == 65        a == 97       0 == 48
+                               //01000001      01100001      00110000
+
+
+        //string collections of data stored between quotation marks
+
+        String schoolName = "lubbock high school";
+
+
+
+
+
+
     }
 }
